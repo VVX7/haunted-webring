55f620527202302e0d99a01063c8d545b79b4fe1
Andrea Brancaleoni <abc@pompel.me>
2023-01-14 01:29:29 +0100

    readd webring.txt and update tsv_to_footer generator

--- tsv_to_footer.rb
+++ tsv_to_footer.rb
@@ -1,15 +1,38 @@
 require 'csv'
 
-puts "<!-- BEGIN GENERATED HAUNTED WEBRING -->"
-puts "<p>This site is part of the <a href=\"https://cse.google.com/cse?cx=f667998e7bdfd4464\" rel=\"noreferrer noopener\" target=\"_blank\">Haunted Webring</a></p>"
-
-def link(href)
-	"<a href=\"#{href}\" rel=\"noreferrer noopener\" target=\"_blank\">#{href}</a>"
+def link(href, title=nil, klass=nil)
+	"<a href=\"#{href}\" #{"class=\"#{klass}\"" if klass}rel=\"noreferrer noopener\" target=\"_blank\">#{title || href}</a>"
 end
+
+return unless ARGV[0] # First argument is your website link
+yoursite = ARGV[0]
+output = []
+ring_previous = nil
+ring_next = nil
+
+rows = CSV.read("webring.tsv", col_sep: "\t").to_a
+
+output << "<p>"
+rows.each_with_index do |row, idx|
+	if yoursite == row[0]
+		ring_previous = rows[idx-1][0]
+		ring_next = rows[idx+1][0]
+	end
+end
+
+rows.each_with_index do |row, idx|
+	output << "\t"+link(row[0], title=nil, klass=("special" if [ring_previous, ring_next].include? row[0])) + ": " + row[1] + "<br />"
+end
+output << "</p>"
+
+puts "<!-- BEGIN GENERATED HAUNTED WEBRING -->"
 
 puts "<p>"
-CSV.read("webring.tsv", col_sep: "\t").each do |row|
-	puts "\t"+link(row[0]) + ": " + row[1] + "<br />"
-end
+puts link(ring_previous, "&lt;"*3, klass="special")
+puts "This site is part of the <a href=\"https://cse.google.com/cse?cx=f667998e7bdfd4464\" rel=\"noreferrer noopener\" target=\"_blank\">Haunted Webring</a>"
+puts link(ring_next, "&gt;"*3, klass="special")
 puts "</p>"
+
+output.each { |o| puts o }
+
 puts "<!-- END GENERATED HAUNTED WEBRING -->"
