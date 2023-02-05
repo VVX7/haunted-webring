6ec11ba106b94b4eb1a267becf9bafff82dd330d
Andrea Brancaleoni <abc@pompel.me>
2023-02-05 03:22:36 +0100

    Initial implementation: GitHub static ring

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# haunted-webring
-a webring for ghosts
+# Haunted Webring
+
+A haunted webring. this repository implements a double-linked list that you can query.
+
+here is the full list of domains in the webring right now: https://github.com/VVX7/haunted-webring/blob/main/webring.txt
+
+usage
+usage: next | previous | random | list
+
+next
+GET /haunted-webring/pixeldreams.tokyo/next
+redirects to the site immediately BEFORE https://pixeldreams.tokyo/
+
+previous
+GET /haunted-webring/pixeldreams.tokyo/previous
+redirects to the site immediately AFTER https://pixeldreams.tokyo/
+
+random
+GET /haunted-webring/pixeldreams.tokyo/random
+redirects to a random site in the ring EXCEPT https://pixeldreams.tokyo/, it requires Javascript
+
+list
+GET /haunted-webring/list
+returns the hard-coded list of domains in the webring in text/plain
+
+html tag
+paste this into your html and modify how you like! (don't forget to edit the domain part)
+
+<p>
+  <a href='https://vvx7.github.io/haunted-webring/<your host>/previous'>&lt;&lt;&lt;</a>
+  this site is part of the <a href='https://vvx7.github.io/haunted-webring/README.md'>[haunted webring]</a> 
+  <a href='https://vvx7.github.io/haunted-webring/<your host>/next'>&gt;&gt;&gt;</a>
+</p>
---
+++ redirect.rb
@@ -0,0 +1,79 @@
+require 'erb'
+require 'optparse'
+require 'ostruct'
+require 'csv'
+require 'fileutils'
+require 'uri'
+
+options = {
+  :output_dir => '_pages/',
+  :webring_file => 'webring.txt'
+}
+OptionParser.new do |opts|
+  opts.banner = "Usage: redirect.rb [options]"
+
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+
+  opts.on("-o", "--output-dir", "Output Directory") do |o|
+    options[:output_dir] = o
+  end
+
+  opts.on("-i", "--webring-file", "WebRing Links file") do |i|
+    options[:webring_file] = i
+  end
+end.parse!
+
+template = <<-EOF
+<!DOCTYPE html>
+<meta charset="utf-8">
+<title>Redirecting to <%= link %></title>
+<meta http-equiv="refresh" content="0; URL=<%= link %>">
+<link rel="canonical" href="<%= link %>">
+EOF
+
+random_template = <<-EOF
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Random redirect for <%= link %></title>
+</head>
+<body>
+  <noscript>
+    Cannot random redirect! Enable Javascript!
+  </noscript>
+  <script>
+    let others = <%= others.to_s %>;
+    location.href = others[Math.floor(Math.random()*others.length)];
+  </script>
+</body>
+</html>
+EOF
+
+def render(t, vars)
+  ERB.new(t).result(OpenStruct.new(vars).instance_eval { binding })
+end
+
+class WrapArray < Array
+  def [](ind)
+    self.fetch(ind%self.length)
+  end
+end
+
+links = WrapArray.new CSV.readlines(options[:webring_file]).map(&:first)
+
+FileUtils.mkdir_p options[:output_dir]
+
+links.each_with_index do |l, idx|
+  host = URI.parse(l).host
+  FileUtils.mkdir_p "#{options[:output_dir]}/#{host}"
+  File.write("#{options[:output_dir]}/#{host}/next.html", render(template, link: links[idx+1]), mode: 'w+')
+  File.write("#{options[:output_dir]}/#{host}/previous.html", render(template, link: links[idx-1]), mode: 'w+')
+  File.write("#{options[:output_dir]}/#{host}/random.html", render(random_template, link: l, others: links - [l]), mode: 'w+')
+end
+
+File.write("#{options[:output_dir]}/list.html", render(template, link: "https://raw.githubusercontent.com/VVX7/haunted-webring/main/webring.txt"), mode: 'w+')
+FileUtils.cp('README.md', options[:output_dir])
